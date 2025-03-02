ee9c80e03169baf9806c6aadd1f6d26d21964bf2
Mindcore M <tima.042007@mail.ru>
2025-03-02 16:03:08 +0600

    hw_5

--- hw_5/todos/admin.py
+++ hw_5/todos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Todo
 # Register your models here.
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'due_date', 'status', 'user')
+    list_filter = ('status', 'due_date')
+    search_fields = ('title', 'description')
+    ordering = ('due_date',)
--- hw_5/todos/models.py
+++ hw_5/todos/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
+
 class Todo(models.Model):
-    title = models.CharField(max_length = 255)
-    description = models.CharField(max_length = 500)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
     due_date = models.DateField()
-    status = models.BooleanField(default = False)
-    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    status = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-
-
